03d244780f7e5d5f688bdb33f593d984368076d3
Patrick Brosset <patrick.brosset@amadeus.com>
2011-12-21 00:05:00 +0100

    Dynamically generating the cookie key at each app start

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,9 @@
+import os
+
 from util.gaesessions import SessionMiddleware
 
 def webapp_add_wsgi_middleware(app):
-	app = SessionMiddleware(app, cookie_key="\xcb\xd2\x9c\xbfZW\xab\xe9\xdb!5L\x80\xa0u\x0b\x05_>\xabn\xd6\xcf\xac`Zi\xe9\x14\xed\x80\xec\x9e\x95|\xd0;g\xa3^\x0ee\x86R\x0f\x90N\xdf\xb0\xbb\xf8\x8e\xd25\xd8\x8e\t\xb084o\x16\x07\x10")
+	# The cookie key will be different everytime the app is deployed, which means existing sessions will be lost.
+	# This is not a big problem for now, and at least avoids sharing the secret with everyone on github
+	app = SessionMiddleware(app, cookie_key=os.urandom(64))
 	return app
